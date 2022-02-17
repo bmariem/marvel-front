77997c75b8df33421613d0eb8365b4d5189b0fb9
bmariem <mariem.borchani@gmail.com>
2022-02-17 23:12:03 +0100

    cloud API

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "https://mariem-marvel-api.herokuapp.com/",
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://mariem-marvel-api.herokuapp.com/",
 });
 
 export default instance;
